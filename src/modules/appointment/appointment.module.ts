e4232f6b0b96c0dab78fcbf929f9217ea0dd709f
jamesMoradi <james.js.dev@outlook.com>
2025-12-25 21:38:47 +0330

    create appointment module

---
+++ src/modules/appointment/appointment.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppointmentService } from './appointment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Appointment, AppointmentSchema } from './schema/appointment.schema';
+import { Doctor, DoctorSchema } from '../doctor/schema/doctor.schema';
+import { Patient, PatientSchema } from '../patient/schema/patient.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Appointment.name, schema: AppointmentSchema },
+      { name: Patient.name, schema: PatientSchema },
+      { name: Doctor.name, schema: DoctorSchema },
+    ]),
+  ],
+  controllers: [],
+  providers: [AppointmentService],
+})
+export class AppointmentModule {}
